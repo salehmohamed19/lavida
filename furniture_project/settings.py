285289d03c43e85db290a34b7c98d1c6627c6d56
salehmohamed19 <salehrabia87@gmail.com>
2026-09-13 21:51:57 +0300

    Finalize Neon database setup with SSL enabled 1111

--- furniture_project/settings.py
+++ furniture_project/settings.py
@@ -8,7 +8,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-furniture-store-key')
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
